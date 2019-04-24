753157b2145c7841baa6913d891be10441c45db6
Jiangming Liu <jmliunlp@gmail.com>
2019-04-24 18:35:26 +0100

    commit

--- onmt/modules/embeddings.py
+++ onmt/modules/embeddings.py
@@ -437,9 +437,6 @@ class BertEmbeddings(nn.Module):
         with torch.no_grad():
             encoded_layers, _ = self.emb_luts(indexed_tokens, attention_mask=masks)
 
-        for i in range(len(encoded_layers)):
-            encoded_layers[i].requires_grad = False
-
         normed_weights = torch.nn.functional.softmax(torch.cat([parameter for parameter
                                                                 in self.scalar_parameters]), dim=0)
         normed_weights = torch.split(normed_weights, split_size_or_sections=1)
